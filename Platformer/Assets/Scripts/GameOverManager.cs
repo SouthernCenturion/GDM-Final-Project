ce1093ac959afa5f3c5a71d87fbf6047ec96713a
SouthernCenturion <leezachary12@icloud.com>
2026-04-05 18:56:04 -0400

    Add SQLite database for high scores

--- Platformer/Assets/Scripts/GameOverManager.cs
+++ Platformer/Assets/Scripts/GameOverManager.cs
@@ -4,11 +4,29 @@ using TMPro;
 
 public class GameOverManager : MonoBehaviour
 {
+    public TMP_InputField playerNameInput;
     public TextMeshProUGUI scoreText;
 
     void Start()
     {
         scoreText.text = "Final Score: " + GameManager.Instance.score;
+    }
+
+    public void OnSubmitScore()
+    {
+        string playerName = playerNameInput.text;
+
+        if (string.IsNullOrEmpty(playerName))
+        {
+            playerName = "Anonymous";
+        }
+
+        int finalScore = GameManager.Instance.score;
+        float completionTime = Time.timeSinceLevelLoad;
+
+        DatabaseManager.Instance.SaveHighScore(playerName, finalScore, completionTime);
+
+        SceneManager.LoadScene("HighScore");
     }
 
     public void TryAgain()
--- Platformer/Assets/Scripts/MenuManager.cs
+++ Platformer/Assets/Scripts/MenuManager.cs
@@ -8,4 +8,9 @@ public class MenuManager : MonoBehaviour
         GameManager.Instance.ResetGame();
         SceneManager.LoadScene(2);
     }
+
+    public void OpenHighScores()
+    {
+    SceneManager.LoadScene(3);
+    }
 }
